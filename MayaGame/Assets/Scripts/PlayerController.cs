5a03278acdf576eddb2c0d3d71c178df410483a2
chloewoods <cwoods29@qub.ac.uk>
2023-10-28 17:31:55 +0100

    player movement and camera
    Basic player movement and camera, this is not great at the minute and will need adjusting to look smooth

--- MayaGame/Assets/Scripts/PlayerController.cs
+++ MayaGame/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed = 1.0f;
+    public float turnSpeed = 10.0f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        //Check if there is a left or right input
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Rotate the player right or left depending on input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        //Check if there is a up or down input
+        verticalInput = Input.GetAxis("Vertical");
+        //Rotate the player up or down depending on input
+        transform.Rotate(Vector3.left, Time.deltaTime * turnSpeed * verticalInput);
+
+        //Player automatically goes forward
+        transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
     }
 }
